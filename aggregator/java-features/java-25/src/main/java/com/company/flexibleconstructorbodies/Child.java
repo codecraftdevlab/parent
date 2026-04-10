b62707ccc29abd30ed1a706090740a831351e5aa
pushkarchauhan91 <pushkarchauhan91@pushkarchauhan91s-MacBook-Air.local>
2026-04-10 11:19:53 +0530

    init commit

---
+++ aggregator/java-features/java-25/src/main/java/com/company/flexibleconstructorbodies/Child.java
@@ -0,0 +1,17 @@
+package com.company.flexibleconstructorbodies;
+
+public class Child extends Parent {
+
+// Before Java 25
+//    Child(String name, int age) {
+//        super(name, age);
+//    }
+
+    Child(String name, int age) {
+        if (age > 18) {
+            System.out.println("Child age is greater than 18");
+        }
+        super(name, age);
+        System.out.println("Child constructor called..");
+    }
+}

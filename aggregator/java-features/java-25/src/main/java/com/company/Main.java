db5c29bdd603e0299bb8b6c596bab28bfef7022f
pushkarchauhan91 <pushkarchauhan91@pushkarchauhan91s-MacBook-Air.local>
2026-04-13 10:59:09 +0530

    java 25

--- aggregator/java-features/java-25/src/main/java/com/company/Main.java
+++ aggregator/java-features/java-25/src/main/java/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.stream.IntStream;
 
 public class Main {
-    public static void main(String[] args) {
+    static void main(String[] args) {
 
         System.out.println("Hello and welcome!");
 
--- aggregator/java-features/java-25/src/main/java/com/company/instancemainmethod/InstanceMainMethod.java
+++ aggregator/java-features/java-25/src/main/java/com/company/instancemainmethod/InstanceMainMethod.java
@@ -1,10 +1,10 @@
-//package com.company.instancemainmethod;
-//
-//public class InstanceMainMethod {
-//
-//    double PI = 3.14;
-//
-//    void main() {
-//        System.out.println("PI is: " + PI);
-//    }
-//}
+package com.company.instancemainmethod;
+
+public class InstanceMainMethod {
+
+    double PI = 3.14;
+
+    void main() {
+        System.out.println("PI is: " + PI);
+    }
+}
